2a4d3696e4c82259ca19b2b1fef94bb01c45b497
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-12-05 16:56:10 +0200

    feat: migrate to cloud mongodb atlas with env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose'
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TrainsModule } from './trains/trains.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/railway'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
     TrainsModule,
   ],
   controllers: [AppController],
